46cfe628b5bf25929269761969b260c3ae53b427
AftabAhmedSoomro <154562779+AftabAhmedSoomro@users.noreply.github.com>
2024-03-07 02:40:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var age = "I am " + 19 + " years old";
+alert(age);
+
+var tracker = "You have visited this site " + 15 + " times";
+alert(tracker);
+
+var birthYear =2004;
+document.write("My birth year is " , birthYear , " <br/>Data type of my declared variable is number <br/>");
+console.log(birthYear);
+console.log(typeof (birthYear));
+
+var visitorName = "John Doe";
+var productTitle = "T-Shirt";
+var quantity = 5;
+document.write(visitorName , " ordered " , quantity , productTitle + "(s) from Daraz");
